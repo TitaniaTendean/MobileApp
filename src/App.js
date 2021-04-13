56db2cbe8ef89ac441ecd5d93d4d77b7aff08142
TitaniaTendean <s21710599@student.unklab.ac.id>
2021-04-13 10:02:12 +0800

    Basic Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './components/pages/Login/index';
+import Register from './components/pages/Register';
+import UserList from './components/pages/User_List';
+
+
+const App = () => {
+  return <UserList />;
+}
+
+export default App;
+
